ec14eb073d495c5370b20a105bdcdc5bbbf6d071
Luca Ferroni <luca@befair.it>
2026-09-21 17:38:48 +0200

    Prepared containers architecture

---
+++ manage_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source $(pwd)/.env
+
+if [ "x$MY_ENV" != "xdev" ]; then
+    echo "Error: MY_ENV for dev environment MUST be set to 'dev'";
+    exit 100;
+fi
+
+COMMAND="docker compose -p $MY_PROJECT -f compose.yml"
+if [ -e "compose.dev.yml" ]; then
+    COMMAND="$COMMAND -f compose.dev.yml"
+fi
+
+echo "Doing $COMMAND $@";
+
+$COMMAND $@
